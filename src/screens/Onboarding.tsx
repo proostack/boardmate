d43ba29b4c89b60d1f78775ecd3836472fc7ed45
Eugene-44hub <eugeneishado@gmail.com>
2022-06-05 02:31:27 +0100

    reusable auth component

--- src/screens/Onboarding.tsx
+++ src/screens/Onboarding.tsx
@@ -2,15 +2,14 @@ import Button  from "../components/Buttons/Button";
 import React from "react";
 import Card from "../components/Card";
 import FormHeader from "../components/Headers/FormHeader";
-import { Box,Heading,Select,Text } from "native-base";
+import { Box,Heading,Text } from "native-base";
 import FaceBook_Google from "../components/FaceBook_Google";
 import InputField from "../components/InputField";
 
 export default function Onboarding() {
-const [email,setEmail]=React.useState("")
-const [password,setPassword]=React.useState("")
-const [show,setShow]=React.useState(true)
-const [language, setLanguage] = React.useState<string>('key0');
+const [email,setEmail]=React.useState<string>("")
+const [password,setPassword]=React.useState<string>("")
+const [show,setShow]=React.useState<boolean>(true)
 
 const setVisibilty=()=>{
     setShow(!show)
@@ -19,24 +18,15 @@ console.log(show)
     return(
         <>
         {/* <Card /> */}
-        {/* <Box mt={71}>  <FormHeader text="testing login component" header="Login"/></Box>
+        <Box mt={71}> 
+         <FormHeader text="testing login component" header="Login"/></Box>
         <InputField input={email} getInput={setEmail} label="Email"/>
         <InputField input={password} getInput={setPassword} label="Password" setVisibilty={setVisibilty} visibility={show}/>
         <Button text="Login"/>
-       <FaceBook_Google/> */}
+       <FaceBook_Google/>
+       
 
-<Select mt={30}
-      placeholder="Mode of payment"
-      selectedValue={language}
-      width={150}
-      onValueChange={(itemValue: string) => setLanguage(itemValue)}
-    >
-      <Select.Item label="Wallet" value="key0" />
-      <Select.Item label="ATM Card" value="key1" />
-      <Select.Item label="Debit Card" value="key2" />
-      <Select.Item label="Credit Card" value="key3" />
-      <Select.Item label="Net Banking" value="key4" />
-    </Select>
+
         </>
     )
 }
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 const userSlice = createSlice({
@@ -15,21 +14,3 @@ const userSlice = createSlice({
 
 export const { setUserDetails } = userSlice.actions
 export default userSlice.reducer
-=======
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-
-const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    user: "first user"
-  },
-  reducers: {
-    setUserDetails(state, action: PayloadAction<string>) {
-      state.user = action.payload
-    }
-  }
-})
-
-export const { setUserDetails } = userSlice.actions
-export default userSlice.reducer
->>>>>>> 97be44104d8b9e7f15c17e7285aa76335b1947ff
