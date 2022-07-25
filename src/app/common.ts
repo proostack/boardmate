feb6ef8d82fea8b4f9dedd8a78f8ae7eb09d0cc7
jbrainz <piusunimke@gmail.com>
2022-07-25 20:19:00 +0100

    fix: refactor imports

---
+++ src/app/common.ts
@@ -0,0 +1,11 @@
+const icons = {
+  Handshake: require("../../assets/images/dashboard/choosePlay/handshake.png"),
+  Die: require("../../assets/images/dashboard/choosePlay/die.png"),
+  Dice: require("../../assets/images/dashboard/games/dice.png"),
+  Draught: require("../../assets/images/dashboard/games/draught.png"),
+  Monopoly: require("../../assets/images/dashboard/games/monopoly.png"),
+  Chess: require("../../assets/images/dashboard/games/chess.png"),
+  Scramble: require("../../assets/images/dashboard/games/scrabble.png"),
+};
+
+export default icons;
